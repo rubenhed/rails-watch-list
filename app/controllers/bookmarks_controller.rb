ac83ea26be4e11c52f3940688ee20febf1e5253f
Ruben HedstrÃom <ruben.hedstrom@hotmail.com>
2025-01-11 15:35:44 +0900

    done no styling

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = List.find(@bookmark.list_id)
+    if @bookmark.destroy
+      redirect_to list_path(@list)
+    else
+      redirect_to list_path(@list)
+    end
+  end
+
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
